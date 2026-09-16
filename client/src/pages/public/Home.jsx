e555193f4dea6aed87b031454eab6c33e143b4f2
Kushal Verma <verma.kushal206@gmail.com>
2026-09-17 00:56:37 +0530

    fix: unmatched JSX tag in Home.jsx

--- client/src/pages/public/Home.jsx
+++ client/src/pages/public/Home.jsx
@@ -147,7 +147,7 @@ const Home = () => {
               </div>
             ))}
           </div>
-        </div>
+        </motion.div>
 
         {/* Scroll indicator */}
         <div className="absolute bottom-8 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2 text-[#6b6b80]">
